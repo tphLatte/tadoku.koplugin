245e73d085bad3f61f3183554ac56dcfba0c0f04
tphLatte <regaladoflorianocc@gmail.com>
2025-11-06 12:06:47 -0600

    new file:   _meta.lua

---
+++ _meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "tadoku",
+    fullname = _("Tadoku Addon"),
+    description = _("Adds a button to the dictionary popup window allowing you to make an Anki card for that word"),
+}
